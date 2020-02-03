a1388166af22d46ecda8cf0b384fe6297a62c4da
zed1025 <amit251098@yahoo.in>
2020-02-04 02:11:57 +0530

    Assignment 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,66 +3,66 @@ import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb
 import {Link} from 'react-router-dom';
 
 
-  function RenderDish({dish}) {
-    if(dish != null) {
-      return (
-        <Card >
-          <CardImg width="100%" object src={dish.image} alt={dish.name}/>
-          <CardBody>
-            <CardTitle heading>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    } else {
-      return (
-        <div></div>
-      );
-    }
+function RenderDish({dish}) {
+  if(dish != null) {
+    return (
+      <Card >
+        <CardImg width="100%" object src={dish.image} alt={dish.name}/>
+        <CardBody>
+          <CardTitle heading>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  } else {
+    return (
+      <div></div>
+    );
   }
+}
 
-  function RenderComments({comments}) {
-    const options = { year: "numeric", month: "short", day: "numeric" };
-    if(comments != null) {
-      return comments.map(comment => (
-        <ul key={comment.id} className="list-unstyled">
-          <li className="mb-2">{comment.comment}</li>
-          <li>
-            -- {comment.author}{" "}
-            {new Date(comment.date).toLocaleDateString("en-US", options)}
-          </li>
-        </ul>
-      ));
-    } else { return ( <div/>); }
-  }
+function RenderComments({comments}) {
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  if(comments != null) {
+    return comments.map(comment => (
+      <ul key={comment.id} className="list-unstyled">
+        <li className="mb-2">{comment.comment}</li>
+        <li>
+          -- {comment.author}{" "}
+          {new Date(comment.date).toLocaleDateString("en-US", options)}
+        </li>
+      </ul>
+    ));
+  } else { return ( <div/>); }
+}
 
 
-  const DishDetail = (props) => {
-    const {dish} = props;
+const DishDetail = (props) => {
+  const {dish} = props;
 
-    return dish?(
-      <div className="container">
-        <div className="row">
-          <Breadcrumb>
-            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-          </Breadcrumb>
-          <div className="col-12">
-            <h3>{props.dish.name}</h3>
-            <hr />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            <RenderDish dish={props.dish}/>
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            <h4>Comments</h4>
-            <RenderComments comments={props.comments}/>
-          </div>
+  return dish?(
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>{props.dish.name}</h3>
+          <hr />
         </div>
       </div>
-    ):(<div></div>);
-  }
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          <RenderDish dish={props.dish}/>
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          <RenderComments comments={props.comments}/>
+        </div>
+      </div>
+    </div>
+  ):(<div></div>);
+}
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import {DISHES} from '../shared/dishes';
 import {COMMENTS} from '../shared/comments';
 import {LEADERS} from '../shared/leaders';
@@ -47,6 +48,8 @@ class Main extends Component{
         <Header/>
         <Switch>
           <Route path="/home" component={HomePage}/>
+          {/* Assignment 2 */}
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>}/>
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
           <Route path="/menu/:dishId" component={DishWithId}/>
           <Route exact path="/contactus" component={Contact} />
